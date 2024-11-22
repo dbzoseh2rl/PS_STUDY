0fad7a192a39de1ab40c7f34d9221e634f2dcc90
psg <1999byh@naver.com>
2024-11-22 09:20:27 +0900

    박성근: ['프로그래머스'] '행렬의 곱셈'_'20241122'

---
+++ src/Programmers12949.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Programmers12949 {
+    public static void main(String[] args) {
+
+        Solution.solution(new int[][]{{2, 3, 2}, {4, 2, 4}, {3, 1, 4}}, new int[][]{{5, 4, 3}, {2, 4, 1}, {3, 1, 1}});
+    }
+}
+
+class Solution {
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
+    }
+}
